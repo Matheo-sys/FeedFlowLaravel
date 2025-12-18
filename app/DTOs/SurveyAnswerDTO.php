32f4eb67e2f56fe70a4f8d2b306bb354b4447681
Matheo-sys <matheosoriano.pro@gmail.com>
2025-12-18 11:18:24 +0100

    Implement us7 : maillable, view, command (8:00), datas

--- app/DTOs/SurveyAnswerDTO.php
+++ app/DTOs/SurveyAnswerDTO.php
@@ -10,13 +10,13 @@ final class SurveyAnswerDTO
 {
     public function __construct(
         public readonly array $answers
-    ) {}
+    ) {
+    }
 
     public static function fromRequest(StoreSurveyAnswerRequest $request): self
     {
-        // On récupère uniquement le tableau validé 'answers'
         return new self(
-            answers: $request->validated('answers') ?? []
+            answers: $request->validated()['answers'] ?? []
         );
     }
 }
